2582df15880d62d4f729c555f95dda9100e5555e
Adam Oommen Jacob <adamoommenjacob@gmail.com>
2021-08-01 11:06:51 +0530

    adding money

--- lib/src/widgets/add_button.dart
+++ lib/src/widgets/add_button.dart
@@ -3,47 +3,49 @@ import 'package:flutter/material.dart';
 class AddButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(right: 10.0),
-      alignment: Alignment.center,
-      padding: EdgeInsets.all(12.0),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(8.0),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.lightBlue.shade50,
-            blurRadius: 8.0,
-            spreadRadius: 4,
-          ),
-          BoxShadow(
-            color: Colors.white,
-            blurRadius: 10.0,
-          ),
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.add_circle,
-              color: Colors.blue,
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.only(right: 10.0),
+        alignment: Alignment.center,
+        padding: EdgeInsets.all(12.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.lightBlue.shade50,
+              blurRadius: 8.0,
+              spreadRadius: 4,
             ),
-            onPressed: () => print("add money"),
-            iconSize: 40.0,
-          ),
-          Text(
-            "Add Person",
-            style: TextStyle(
-                inherit: true,
-                fontWeight: FontWeight.bold,
-                fontSize: 14.0,
-                color: Colors.black45),
-            textAlign: TextAlign.center,
-          ),
-        ],
+            BoxShadow(
+              color: Colors.white,
+              blurRadius: 10.0,
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.add_circle,
+                color: Colors.blue,
+              ),
+              onPressed: () => print("add money"),
+              iconSize: 40.0,
+            ),
+            Text(
+              "Add Person",
+              style: TextStyle(
+                  inherit: true,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14.0,
+                  color: Colors.black45),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
       ),
     );
   }
